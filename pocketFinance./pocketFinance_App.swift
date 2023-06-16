333e09dcfc1314d7abb900706531c6284480dde2
Kylie Nguyen <nguyenm.kylie@gmail.com>
2023-06-15 17:26:35 -0700

    Initial Commit

---
+++ pocketFinance./pocketFinance_App.swift
@@ -0,0 +1,17 @@
+//
+//  pocketFinance_App.swift
+//  pocketFinance.
+//
+//  Created by Kylie Nguyen on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct pocketFinance_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
